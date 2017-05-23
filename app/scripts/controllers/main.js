4c530659905b82829065a1309a7ca0fc08ee8fd4
phuud <trunkb@gmail.com>
2017-05-23 21:27:25 +0800

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nextDinnerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nextDinnerApp
+ */
+angular.module('nextDinnerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
