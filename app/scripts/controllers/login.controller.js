3c508e038ce63ec21ef8d8ca25428ba289e1637b
phuud <trunkb@gmail.com>
2017-06-01 20:59:33 +0800

    update login part.

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -4,65 +4,28 @@
 
 angular.module('nextDinnerApp')
   .controller('LoginCtrl', function ($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
-    $scope.submit = function () {
-      check_login();
-      return false;
-    }
-    function check_login()
-    {
-      var name=$("#user_name").val();
-      var pass=$("#password").val();
-      if(name=="www.htmleaf.com" && pass=="www.htmleaf.com")
-      {
-        alert("登录成功！");
-        $("#user_name").val("");
-        $("#password").val("");
-
-      }
-      else
-      {
-        $("#login_form").removeClass('shake_effect');
-        setTimeout(function()
-        {
-          $("#login_form").addClass('shake_effect')
-        },1);
+    $scope.main = {
+      init: function () {
+        this.title = 'Next OA'
+      },
+      submit: function () {
+        var name = $("#user_name").val();
+        var pass = $("#password").val();
+        if (name == "luke" && pass == "111111") {
+          alert("登录成功！");
+          $("#user_name").val("");
+          $("#password").val("");
+        }
+        else {
+          $("#login_form").removeClass('shake_effect');
+          setTimeout(function () {
+            $("#login_form").addClass('shake_effect')
+          }, 1);
+        }
+      },
+      register: function () {
+        alert('未开放注册');
       }
     }
-    function check_register(){
-      var name = $("#r_user_name").val();
-      var pass = $("#r_password").val();
-      var email = $("r_email").val();
-      if(name!="" && pass=="" && email != "")
-      {
-        alert("注册成功！");
-        $("#user_name").val("");
-        $("#password").val("");
-      }
-      else
-      {
-        $("#login_form").removeClass('shake_effect');
-        setTimeout(function()
-        {
-          $("#login_form").addClass('shake_effect')
-        },1);
-      }
-    }
-    $(function(){
-      $("#create").click(function(){
-        check_register();
-        return false;
-      })
-      $('.message a').click(function () {
-        $('form').animate({
-          height: 'toggle',
-          opacity: 'toggle'
-        }, 'slow');
-      });
-    })
+    $scope.main.init();
   });
