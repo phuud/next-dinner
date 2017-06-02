3008709365cf20f056028bf5927f0e1be7a10c33
phuud <trunkb@gmail.com>
2017-06-02 18:16:12 +0800

    init angular-material

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngMaterial'
   ])
   .run(function ($rootScope,$route,$location) {
     var APP_ID = 'TODO';
