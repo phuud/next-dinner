2bd6eb3691eb474c199d013367ac5c79b3bb045a
phuud <trunkb@gmail.com>
2017-06-05 18:55:28 +0800

    update login page.

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -4,41 +4,6 @@
 
 angular.module('nextDinnerApp')
   .controller('LoginCtrl', function ($scope) {
-    var animating = false, submitPhase1 = 1100, submitPhase2 = 400, logoutPhase1 = 800, $login = $('.login'), $app = $('.app');
-    function ripple(elem, e) {
-      $('.ripple').remove();
-      var elTop = elem.offset().top, elLeft = elem.offset().left, x = e.pageX - elLeft, y = e.pageY - elTop;
-      var $ripple = $('<div class=\'ripple\'></div>');
-      $ripple.css({
-        top: y,
-        left: x
-      });
-      elem.append($ripple);
-    }
-    ;
-    $(document).on('click', '.login__submit', function (e) {
-      if (animating)
-        return;
-      animating = true;
-      var that = this;
-      ripple($(that), e);
-      $(that).addClass('processing');
-      setTimeout(function () {
-        $(that).addClass('success');
-        setTimeout(function () {
-          $app.show();
-          $app.css('top');
-          $app.addClass('active');
-        }, submitPhase2 - 70);
-        setTimeout(function () {
-          $login.hide();
-          $login.addClass('inactive');
-          animating = false;
-          $(that).removeClass('success processing');
-        }, submitPhase2);
-      }, submitPhase1);
-    });
-
     $scope.main = {
       init: function () {
         this.title = 'Next OA'
@@ -62,7 +27,25 @@ angular.module('nextDinnerApp')
       },
       register: function () {
         alert('未开放注册');
+      },
+      join: function () {
+        var t = 0;
+        if (t == 0) {
+          document.querySelectorAll('.cont_letras > p')[0].style.left = '200px';
+          document.querySelectorAll('.cont_letras > p')[1].style.left = '-320px';
+          document.querySelectorAll('.cont_letras > p')[2].style.left = '200px';
+          setTimeout(function () {
+            document.querySelector('.cont_join').className = 'cont_join cont_join_form_act';
+          }, 1000);
+          t++;
+        } else {
+          t++;
+          document.querySelector('.cont_form_join').style.bottom = '-420px';
+          setTimeout(function () {
+              document.querySelector('.cont_join').className = 'cont_join cont_join_form_act cont_join_finish';
+            }, 500);
+        }
       }
-    }
+    };
     $scope.main.init();
   });
