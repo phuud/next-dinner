987613fd370423fa1cdeeac98b9a2578a45fe72e
phuud <trunkb@gmail.com>
2017-06-06 20:15:19 +0800

    update login page.

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -31,18 +31,16 @@ angular.module('nextDinnerApp')
       join: function () {
         var t = 0;
         if (t == 0) {
-          document.querySelectorAll('.cont_letras > p')[0].style.left = '200px';
-          document.querySelectorAll('.cont_letras > p')[1].style.left = '-320px';
-          document.querySelectorAll('.cont_letras > p')[2].style.left = '200px';
+          angular.element('.login-head p').css('left',320);
           setTimeout(function () {
-            document.querySelector('.cont_join').className = 'cont_join cont_join_form_act';
+            angular.element('.login-box').addClass('login-act');
           }, 1000);
           t++;
         } else {
           t++;
-          document.querySelector('.cont_form_join').style.bottom = '-420px';
+          document.querySelector('.login-content').style.bottom = '-420px';
           setTimeout(function () {
-              document.querySelector('.cont_join').className = 'cont_join cont_join_form_act cont_join_finish';
+              document.querySelector('.login-box').className = 'login-box login-act login-finish';
             }, 500);
         }
       }
