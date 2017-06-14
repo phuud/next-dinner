69e3f990e3d31e2098a028c6d5296e2bdebae51f
phuud <trunkb@gmail.com>
2017-06-14 18:55:40 +0800

    Add loading/update view style.

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -3,38 +3,46 @@
  */
 
 angular.module('nextDinnerApp')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope, $timeout, $location) {
     $scope.main = {
       init: function () {
         this.title = 'Next OA'
       },
       flag:{
         isJoinTab:true,
-        isLoginTab:false
+        isLoginTab:false,
+        showLoading:false
       },
       join: function () {
         var that = this;
         angular.element('.login-head p').css('left',320);
-        setTimeout(function () {
+        $timeout(function () {
           angular.element('.login-box').addClass('login-act');
         }, 200);
         that.flag.isLoginTab = true;
         that.flag.isJoinTab = false;
       },
       submit: function () {
+        var that = this;
+        that.flag.showLoading = true;
         var name = $("#user_name").val();
         var pass = $("#password").val();
-        console.log("is login");
         AV.User.logIn(name, pass).then(function (loginedUser) {
           console.log(loginedUser);
+          that.flag.showLoading = false;
           var username = loginedUser.getUsername();
           var email = loginedUser.getEmail();
-          alert("登录成功！");
+          //alert("登录成功！");
           angular.element('.login-content').css('top','-250px');
-          setTimeout(function () {
+          $timeout(function () {
             angular.element('.login-box').addClass('login-finish');
+            $timeout(function () {
+              angular.element('.login-box').addClass('login-finish');
+                $location.path("/dinner")
+            }, 2000);
           }, 500);
         }, function (error) {
+          that.flag.showLoading = false;
           $("#user_name").val("");
           $("#password").val("");
           $(".login-box").removeClass('shake_effect');
