93ce2baaacb13b446f39b33e5abbe6929d9d6605
phuud <trunkb@gmail.com>
2017-06-16 23:47:48 +0800

    Add config.

---
+++ app/scripts/app.config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Luke on 2017/6/16.
+ */
+"use strict";
+angular.module('nextDinnerApp')
+
+  .constant('ENV', 'dev')
+
+  .constant('VERSION', 'v0.0.1')
+
+  .constant('LEANCLOUD_APP_ID', 'xxx')
+  .constant('LEANCLOUD_APP_KEY', 'xxx')
+;
