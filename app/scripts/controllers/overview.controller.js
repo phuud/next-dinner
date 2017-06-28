154010c9c5d5091020efdf0fd489d8f8fa738e61
phuud <trunkb@gmail.com>
2017-06-28 19:05:32 +0800

    Update the view style of overview.

--- app/scripts/controllers/overview.controller.js
+++ app/scripts/controllers/overview.controller.js
@@ -7,56 +7,6 @@
 angular.module('nextDinnerApp')
   .controller('OverviewCtrl', function ($scope) {
     $scope.overview = {
-      links: [
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        },
-        {
-          linkName: 'NextWeb',
-          linkUrl: 'www.nexttrucking.com'
-        }
-      ],
       init: function () {
         console.log(this.links);
         $scope.datas = this.links;
