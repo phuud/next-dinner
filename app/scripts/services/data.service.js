81f213b33387fbc68bd5557b7fa2cbad741232f7
phuud <trunkb@gmail.com>
2017-07-05 23:45:24 +0800

    Update data service.

--- app/scripts/services/data.service.js
+++ app/scripts/services/data.service.js
@@ -9,18 +9,28 @@ angular.module('nextDinnerApp')
     return {
       linkData: function(){
         var linkData = [
+          {
+            title: 'Google',
+            text: 'google site',
+            url: 'https://www.google.com'
+          },
             {
               title: 'NextTrucking',
-              text: 'Website',
+              text: 'Home page',
               url: 'https://www.nexttrucking.com'
             },
             {
-              title: 'Google',
-              text: 'google site',
-              url: 'https://www.google.com'
+              title: '邮箱',
+              text: '网易企业邮箱',
+              url: 'https://qiye.aliyun.com/'
             }
           ];
         return linkData;
+      },
+      getData: function () {
+        return $http.get("/api/getId/" + id).then(function (response) {
+          return {"data": response.data, "status": response.status, "headers": response.headers};
+        });
       }
-    }
+    };
   });
